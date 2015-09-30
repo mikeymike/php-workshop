c5a36e7fca4e3c03bee364738bf095d42145ba06
Aydin Hassan <aydin@hotmail.co.uk>
2015-09-30 01:14:28 +0100

    wip

--- md-test.php
+++ md-test.php
@@ -1,16 +1,53 @@
 <?php
 
+ini_set('display_errors', 1);
 require_once 'vendor/autoload.php';
 
+use AydinHassan\CliMdRenderer\CliRendererFactory;
 use League\CommonMark\DocParser;
 use League\CommonMark\Environment;
 
 $c = new \Colors\Color();
 $c->setForceStyle(true);
 
-$docParser = new DocParser(Environment::createCommonMarkEnvironment());
-$ast = $docParser->parse(file_get_contents('res/problems/baby-steps/problem.md'));
-$children = $ast->children();
+$parser = new DocParser(Environment::createCommonMarkEnvironment());
+$exercises = glob('res/problems/*');
+$numExercises = count($exercises);
 
-$cliRender = new \PhpWorkshop\PhpWorkshop\Md\CliRenderer($c);
-echo $cliRender->renderBlock($ast);
+$cliRender = (new CliRendererFactory)->__invoke();
+
+$helpLine = function ($text, $cmd) use ($c) {
+    $cmd = $c(sprintf('php %s %s', $_SERVER['argv'][0], $cmd))->yellow()->__toString();
+    return sprintf(
+        " %s %s: %s\n",
+        $c("»")->bold()->__toString(),
+        $text,
+        $cmd
+    );
+};
+
+$output = function ($exercise, $index) use ($c, $parser, $numExercises, $cliRender, $helpLine) {
+    $title = strtoupper(str_replace("-", ' ', basename($exercise)));
+    $content = file_get_contents($exercise . "/problem.md");
+
+    echo "\n";
+    echo $c(' LEARN YOU THE PHP FOR MUCH WIN! ')->green()->bold() . "\n";
+    echo $c('─────────────────────────────────')->green()->bold() . "\n";
+    echo $c(" $title")->yellow()->bold() . "\n";
+    echo $c(" Exercise $index of $numExercises")->yellow() . "\n\n";
+
+    $ast = $parser->parse($content);
+    echo $cliRender->renderBlock($ast);
+
+    echo "\n";
+    echo $helpLine('To print these instructions again, run', 'print');
+    echo $helpLine('To execute your program in a test environment, run', 'run program.php');
+    echo $helpLine('To verify your program, run', 'verify program.php');
+    echo $helpLine('For help run', 'help');
+    echo "\n\n";
+
+};
+
+foreach ($exercises as $index => $exercise) {
+    $output($exercise, $index);
+}
